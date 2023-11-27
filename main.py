6a450f175f58102a376d95c33bc7fd9ba60df1ea
Otunla Timilehin <69770115+Duade10@users.noreply.github.com>
2023-11-27 20:54:26 +0100

    Update

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ app = FastAPI()
 
 class ActivationMailModel(BaseModel):
     to_mail: str
-    activation_url: str
-    template_code: str
+    activation_url: str = None
+    template_code: str = None
+
 
 
 @app.get("/")
@@ -35,3 +36,29 @@ async def send_activation_mail(item: ActivationMailModel):
     print(response.status_code)
     print(response.text)
     return response.status_code
+
+class WelcomeMailModel(BaseModel):
+    recipient_email: str
+    template_code: str
+
+
+@app.post("/send-welcome-mail")
+async def send_welcome_mail(item: WelcomeMailModel):
+    api_key = "SGFrZWVtLTM4NDE1MzQ4OTc3NS4xMTExNS0xMjU="
+    url = "https://mailing.go-mailer.com/api/v1/transactionals/dispatch"
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+    }
+    payload = {
+        "template_code": item.template_code,
+        "recipient_email": item.recipient_email,
+        "data": {
+            "first_name,": "Otunla"
+        }
+    }
+    response = requests.post(url, headers=headers, data=payload)
+    print(response.status_code)
+    print(response.text)
+    return response.
+    
+@app
